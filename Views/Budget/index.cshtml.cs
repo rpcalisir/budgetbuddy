183336981f3c1e4035bf5bf683ec0a66a9f9c81e
Recep Çalışır <55884180+rpcalisir@users.noreply.github.com>
2025-02-14 10:26:52 +0300

    Add files via upload

---
+++ Views/Budget/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgetBuddy.Web.Views.Budget
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
