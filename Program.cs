ea92fbac36ad826d974ee9fc0818309d5f8a88e9
Recep Çalışır <55884180+rpcalisir@users.noreply.github.com>
2025-02-19 15:41:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+
+namespace BudgetBuddy.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // Register all MediatR handlers from the current assembly.
+            // This allows MediatR to automatically discover and inject handlers for your commands and queries.
+            // In a Clean Architecture solution, you would typically point this to your Application layer assembly
+            // where all your handlers are located.
+            //builder.Services.AddApplicationServices(builder.Configuration);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
